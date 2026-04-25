1e63d347d6efef46785ec5132b496cc4cd8df07f
Ryosuke Fujii <inf.ryo@gmail.com>
2026-04-25 14:23:09 +0900

    add chat script tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -11,7 +12,16 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="ja">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Script
+          async
+          src="https://chat-embedding-widget.pages.dev/widget.v1.js"
+          data-project-key="pk_bf5865306c6d49c18311df06"
+          data-api-origin="https://chatbase-ops.vercel.app"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
